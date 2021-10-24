c7703e94a13c9e8d5dfa75d6032f736b68c74dd0
Ernest G. Wilson II <ErnestGWilsonII@gmail.com>
2021-10-24 16:47:17 -0400

    fix: Updated Group Favorites installer

--- add_group_favorites.sh
+++ add_group_favorites.sh
@@ -17,6 +17,7 @@
 # Add the update copy script
 cd /usr/local/sbin
 cp /usr/local/sbin/supermon-syncgrpfavs.sh /usr/local/sbin/supermon-syncgrpfavs.sh.$(date '+%Y-%m-%d_%H:%M:%S') && rm -rf /usr/local/sbin/supermon-syncgrpfavs.sh
+cd /usr/local/sbin
 wget https://raw.githubusercontent.com/ernestgwilsonii/EchoLink-AllStarLink/main/usr/local/sbin/supermon-syncgrpfavs.sh
 chmod a+rx /usr/local/sbin/supermon-syncgrpfavs.sh
 
