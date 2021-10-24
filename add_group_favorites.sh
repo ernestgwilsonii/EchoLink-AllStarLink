a5f4ea8e803cb00ee582bafea4aadeb034e50ab7
Ernest G. Wilson II <ErnestGWilsonII@gmail.com>
2021-10-24 16:17:39 -0400

    chore: Added Group Favorites installer

---
+++ add_group_favorites.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+#######################################################################################################################
+# Adds RadioFan.Club "Group Favorites" to HamVoIP AllStarLink node on Raapberry Pi                                    #
+#                     Group Favorites hack add-on by: Ryan - https://github.com/MrMophandle                           #
+#                                                                                                                     #
+#  Usage:                                                                                                             #
+#   ssh -p 222 root@YourPiIPAddressHere                                                                               #
+#   cd /tmp                                                                                                           #
+#   curl https://raw.githubusercontent.com/ernestgwilsonii/EchoLink-AllStarLink/main/add_group_favorites.sh | bash -  #
+#                                                                                                                     #
+#                                                                                ErnestGWilsonII@gmail.com 2021-10-24 #
+#                                                                                  https://github.com/ernestgwilsonii #
+#                                                                         https://www.linkedin.com/in/ernestgwilsonii #
+#######################################################################################################################
+
+# Add the update copy script
+cd /usr/local/bin
+wget https://raw.githubusercontent.com/ernestgwilsonii/EchoLink-AllStarLink/main/usr/local/sbin/supermon-syncgrpfavs.sh
+chmod a+rx /usr/local/sbin/supermon-syncgrpfavs.sh
+
+# Backup original files
+cd /srv/http/supermon
+cp /srv/http/supermon/grpfavorites.php /srv/http/supermon/grpfavorites.php.$(date '+%Y-%m-%d_%H:%M:%S')
+cp /srv/http/supermon/header.inc /srv/http/supermon/header.inc.$(date '+%Y-%m-%d_%H:%M:%S')
+cp /srv/http/supermon/link.php /srv/http/supermon/link.php.$(date '+%Y-%m-%d_%H:%M:%S')
+cp /srv/http/supermon/syncgrpfavorites.php /srv/http/supermon/syncgrpfavorites.php.$(date '+%Y-%m-%d_%H:%M:%S')
+
+# Download Ryan's modified files
+cd /srv/http/supermon
+wget https://raw.githubusercontent.com/ernestgwilsonii/EchoLink-AllStarLink/main/srv/http/supermon/grpfavorites.php
+wget https://raw.githubusercontent.com/ernestgwilsonii/EchoLink-AllStarLink/main/srv/http/supermon/header.inc
+wget https://raw.githubusercontent.com/ernestgwilsonii/EchoLink-AllStarLink/main/srv/http/supermon/link.php
+wget https://raw.githubusercontent.com/ernestgwilsonii/EchoLink-AllStarLink/main/srv/http/supermon/syncgrpfavorites.php
+
+# Set permissions
+cd /srv/http/supermon
+chown root:http /srv/http/supermon/grpfavorites.php
+chown root:http /srv/http/supermon/header.inc
+chown root:http /srv/http/supermon/link.php
+chown root:http /srv/http/supermon/syncgrpfavorites.php
+chmod a+r /srv/http/supermon/grpfavorites.php
+chmod a+r /srv/http/supermon/header.inc
+chmod a+r /srv/http/supermon/link.php
+chmod a+r /srv/http/supermon/syncgrpfavorites.php
